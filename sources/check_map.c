69d1703522d975562c07884759d47114089d8984
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-09 19:18:32 -0300

    Create check_map.c

---
+++ sources/check_map.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_map.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 23:17:55 by acesar-l          #+#    #+#             */
+/*   Updated: 2022/03/09 23:18:17 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/libft.h"
+#include "../header/so_long.h"
+
+int check_map(t_map *map)
+{
+	int 	i;
+
+	i = 0;
+	while(i <= map->nbr_lines)
+	{
+		if (ft_strlen(map->line[i]) != map->line_len)
+			return (ERROR);
+		if (map->line[i][0] != WALL || map->line[i][map->line_len - 1] != WALL)
+			return (ERROR);
+		i++;
+	}
+	i = 0;
+	while (map->line[0][i])
+	{
+		if (map->line[0][i] != WALL || map->line[map->nbr_lines][i] != WALL)
+			return (ERROR);
+		i++;
+	}
+	if (are_mapparameters_present(map) == ERROR)
+		return (ERROR);
+	return (1);
+}
