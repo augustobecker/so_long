d672b12a9a62fb697a7c40190ad505671cd0b507
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-09 19:16:40 -0300

    Create init_map.c

---
+++ sources/init_map.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 23:16:08 by acesar-l          #+#    #+#             */
+/*   Updated: 2022/03/09 23:16:09 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/libft.h"
+#include "../header/so_long.h"
+
+int init_map(t_map *map, char *argv)
+{
+	char 	*read_str;
+	char	*temporary;
+	int		i;
+
+	i = 0;
+	map->fd = open(argv, O_RDONLY);
+	if (map->fd == -1)
+		return (ERROR);
+	read_str = ft_strdup(get_next_line(map->fd));
+	if (!read_str)
+		return (ERROR);
+	temporary = ft_strdup("");
+	while (read_str)
+	{
+		temporary = ft_strjoin(temporary, read_str);
+		read_str = get_next_line(map->fd);
+	}
+	map->line = ft_split(temporary, '\n');
+	map->nbr_lines = ft_count_occurrences(temporary, '\n');
+	map->line_len = ft_strlen(map->line[0]);
+	return (1);
+}
