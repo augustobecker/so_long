75408c7b8cc9f72727052729a195a6948b68dddf
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-10 02:59:07 -0300

    Update check_map.c

--- sources/check_map.c
+++ sources/check_map.c
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 23:17:55 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/09 23:18:17 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/03/10 02:18:17 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,20 @@ int check_map(t_map *map)
 	while(i <= map->nbr_lines)
 	{
 		if (ft_strlen(map->line[i]) != map->line_len)
-			return (ERROR);
+			error_message("The Map must be rectangular!");
 		if (map->line[i][0] != WALL || map->line[i][map->line_len - 1] != WALL)
-			return (ERROR);
+			error_message("The Map must be surrounded by walls! Missing vertical wall");
 		i++;
 	}
 	i = 0;
 	while (map->line[0][i])
 	{
 		if (map->line[0][i] != WALL || map->line[map->nbr_lines][i] != WALL)
-			return (ERROR);
+			error_message("The Map must be surrounded by walls! Missing horizontal wall");
 		i++;
 	}
-	if (are_mapparameters_present(map) == ERROR)
-		return (ERROR);
+	if (map->nbr_lines == map->line_len)
+		error_message("The Map must be rectangular!");
+	are_mapparameters_present(map);
 	return (1);
 }
