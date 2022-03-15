406be6dcab239e594269f475feb669778d30fd72
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-15 12:04:43 -0300

    Update are_mapparameters_present.c

--- sources/are_mapparameters_present.c
+++ sources/are_mapparameters_present.c
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 18:09:43 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/10 20:09:44 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/03/15 12:09:44 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,12 @@ void	are_mapparameters_present(t_map *map, t_game *game)
 		game->coin += ft_count_occurrences(map->line[i], COIN);
 		game->map_exit += ft_count_occurrences(map->line[i], MAP_EXIT);
 		game->start_pos += ft_count_occurrences(map->line[i], START_POS);
-		if (game->coin && game->map_exit && game->start_pos)
-			break ;
 		i++;
 	}
 	if (!game->coin)
 		error_msg("There are no collectibles");
 	if (!game->map_exit)
 		error_msg("There are no Exit");
-	if (!game->start_pos)
+	if (game->start_pos != 1)
 		error_msg("Invalid player quantity");
 }
