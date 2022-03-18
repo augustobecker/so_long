0383836166d49a201dacaa64a0f7160e4200a680
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-18 19:50:03 -0300

    Update and rename check_arguments.c to check_command_line_arguments.c

--- sources/check_command_line_arguments.c
+++ sources/check_command_line_arguments.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check_arguments.c                                  :+:      :+:    :+:   */
+/*   check_command_line_arguments.c                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 07:08:26 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/10 07:08:48 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/03/18 19:48:48 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/libft.h"
 #include "../header/so_long.h"
 
-int check_arguments(int argc, char **argv)
+void check_command_line_arguments(int argc, char **argv)
 {
 	int map_parameter_len;
 
@@ -24,5 +24,4 @@ int check_arguments(int argc, char **argv)
 	map_parameter_len = ft_strlen(argv[1]);
 	if (!ft_strnstr(&argv[1][map_parameter_len - 4], ".ber", 4))
 		error_msg("Map file extention is wrong (It should be .ber)");
-	return (1);
 }
