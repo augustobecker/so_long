7c591dbe9880ace6d4b1c87f6974852906efc3e9
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-21 14:48:03 -0300

    Update check_map.c

--- sources/check_map.c
+++ sources/check_map.c
@@ -6,34 +6,34 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 23:17:55 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/18 19:51:17 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/03/21 16:51:17 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/libft.h"
 #include "../header/so_long.h"
 
-void check_map(t_map *map, t_game *game)
+void check_map(t_game *game)
 {
 	int 	i;
 
 	i = 0;
+	if (game->map_nbr_lines == game->map_line_len)
+		error_msg("Square Map. The Map must be rectangular!");	
 	while(i <= map->nbr_lines)
 	{
-		if (ft_strlen(map->line[i]) != map->line_len)
+		if (ft_strlen(game->map[i]) != game->map_line_len)
 			error_msg("The Map must be rectangular!");
-		if (map->line[i][0] != WALL || map->line[i][map->line_len - 1] != WALL)
+		if (game->map[i][0] != WALL || game->map[i][game->map_line_len - 1] != WALL)
 			error_msg("The Map must be surrounded by walls! Missing vertical wall");
 		i++;
 	}
 	i = 0;
-	while (map->line[0][i])
+	while (i < game->map_line_len)
 	{
-		if (map->line[0][i] != WALL || map->line[map->nbr_lines][i] != WALL)
+		if (game->map[0][i] != WALL || game->line[game->map_nbr_lines][i] != WALL)
 			error_msg("The Map must be surrounded by walls! Missing horizontal wall");
 		i++;
 	}
-	if (map->nbr_lines == map->line_len)
-		error_msg("Square Map. The Map must be rectangular!");
-	check_map_parameters(map, game);
+	check_map_parameters(game);
 }
