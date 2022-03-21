85f544952772180b245d294b305f0ad74ef36269
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-21 14:13:27 -0300

    Update init_map.c

--- sources/init_map.c
+++ sources/init_map.c
@@ -6,31 +6,32 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 23:16:08 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/18 19:56:09 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/03/21 14:56:09 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/libft.h"
 #include "../header/so_long.h"
 
-void init_map(t_map *map, char *argv)
+void init_map(t_game *game, char *argv)
 {
 	char 	*read_str;
 	char	*temporary;
-	int		i;
+	int	map_fd;
+	int	i;
 
 	i = 0;
-	map->fd = open(argv, O_RDONLY);
-	if (map->fd == -1)
+	map_fd = open(argv, O_RDONLY);
+	if (map_fd == -1)
 		error_msg("The Map couldn't be opened. Invalid fd");
-	read_str = ft_strdup(get_next_line(map->fd));
+	read_str = ft_strdup(get_next_line(map_fd));
 	if (!read_str)
 		error_msg("Allocation failed");
 	temporary = ft_strdup("");
 	while (read_str)
 	{
 		temporary = ft_strjoin(temporary, read_str);
-		read_str = get_next_line(map->fd);
+		read_str = get_next_line(map_fd);
 	}
 	map->line = ft_split(temporary, '\n');
 	map->nbr_lines = ft_count_occurrences(temporary, '\n');
