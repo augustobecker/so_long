35ba15b8b1d36cf1f936698f1d5805e9a8112c67
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-24 13:19:32 -0300

    Update check_map_parameters.c

--- sources/check_map_parameters.c
+++ sources/check_map_parameters.c
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 18:09:43 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/23 13:28:44 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/03/24 13:28:44 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	check_map_parameters(t_game *game)
 	{
 		if (check_for_invalid_map_parameters(game->map.line[i]) == TRUE)
 			error_msg("Not expected map parameter");
-		game->map.coins += ft_count_occurrences(game->map.line[i], COIN);
+		game->map.coins += ft_count_occurrences(game->map.line[i], COINS);
 		game->map.exits += ft_count_occurrences(game->map.line[i], MAP_EXIT);
 		game->map.nbr_players += ft_count_occurrences(game->map.line[i], START_POS);
 		if (game->map.nbr_players == 1)
@@ -32,8 +32,8 @@ void	check_map_parameters(t_game *game)
 	if (game->map.coins == 0)
 		error_msg("Invalid Map. There are no collectibles!");
 	else if (game->map.exit == 0)
-		error_msg("Invalid Map. There are no Exit");
+		error_msg("Invalid Map. There is no Exit!");
 	else if (game->map.nbr_players != 1)
-		error_msg("Invalid Map. Invalid player quantity");
+		error_msg("Invalid Map. Invalid player quantity!");
 }
 
