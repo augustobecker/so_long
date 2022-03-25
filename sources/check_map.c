491dd1b630634113ef1c6b1419d2e635f384ceeb
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-25 14:58:20 -0300

    Update check_map.c

--- sources/check_map.c
+++ sources/check_map.c
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 23:17:55 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/24 13:04:17 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/03/25 15:04:17 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,15 @@ void check_map(t_game *game)
 	int 	i;
 
 	i = 0;
+
 	if (game->map.nbr_lines == game->map.line_len)
 		error_msg("Square Map. The Map must be rectangular!");	
-	while(i <= game->map.nbr_lines)
+	while(i < game->map.nbr_lines)
 	{
-		if (ft_strlen(game->map.line[i]) != game->map.line_len)
+		if ((int) strlen_line(game->map.line[i]) != game->map.line_len)
 			error_msg("The Map must be rectangular!");
 		if ((game->map.line[i][0] != WALL)
-				|| (game->map.line[i][game->map.line_len - 1] != WALL))
+			|| (game->map.line[i][game->map.line_len - 1] != WALL))
 			error_msg("The Map must be surrounded by walls! Missing vertical wall.");
 		i++;
 	}
@@ -33,7 +34,7 @@ void check_map(t_game *game)
 	while (i < game->map.line_len)
 	{
 		if ((game->map.line[0][i] != WALL) 
-			|| (game->map.line[game->map.nbr_lines][i] != WALL))
+			|| (game->map.line[game->map.nbr_lines - 1][i] != WALL))
 			error_msg("The Map must be surrounded by walls! Missing horizontal wall.");
 		i++;
 	}
