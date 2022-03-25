01dbadb625d7ad1ac54f6d55064e77f62a2a7416
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-25 12:18:00 -0300

    Update init_vars.c

--- sources/init_vars.c
+++ sources/init_vars.c
@@ -6,16 +6,16 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 15:19:36 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/22 15:32:06 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/03/25 12:21:06 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/so_long.h"
 
-void init_vars(t_game game)
+void init_vars(t_game *game)
 {
-	game->map.coin = 0;
+	game->map.coins = 0;
 	game->map.exit = 0;
-	game->map.player = 0;
+	game->map.nbr_players = 0;
 	game->movements = 0;
 }
