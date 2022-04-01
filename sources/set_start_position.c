6957384587919b29b9b0dec3a2749ed875c50882
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-04-01 14:14:14 -0300

    Update set_start_position.c

--- sources/set_start_position.c
+++ sources/set_start_position.c
@@ -6,20 +6,21 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 13:12:44 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/25 15:13:09 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/04/01 14:13:09 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libraries/Libft/libft.h"
-#include "../header/so_long.h"
+#include "so_long.h"
 
 void set_start_position(t_game *game, char *line, int y)
 {
 	int x;
 
 	x = 0;
-	while(line[x] != START_POS)
+	while(line[x] && line[x] != PLAYER)
 		x++;
+	if (line[x] == '\0')
+		return ;
 	game->map.player.x = x;
 	game->map.player.y = y;
 }
