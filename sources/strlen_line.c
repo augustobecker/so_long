00ee6d42a21b8f367888f82959b57102797ea4eb
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-04-01 14:33:20 -0300

    Update strlen_line.c

--- sources/strlen_line.c
+++ sources/strlen_line.c
@@ -6,18 +6,18 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 10:19:25 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/25 11:27:27 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/04/01 14:27:27 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../header/libft.h"
+#include "so_long.h"
 
 size_t strlen_line(char *str)
 {
 	size_t	length;
 
 	length = 0;
-	while ((str[length]) && (ft_isprint(str[length]) == TRUE))
+	while ((str[length]) && (ft_isprint(str[length]))
 		length++;
 	return (length);
 }
