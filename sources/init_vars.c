83d3e0698c1c128a78cb17646d2431151d5e9754
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-04-02 07:15:12 -0300

    Update init_vars.c

--- sources/init_vars.c
+++ sources/init_vars.c
@@ -6,11 +6,11 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 15:19:36 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/25 12:21:06 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/04/02 07:21:06 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../header/so_long.h"
+#include "so_long.h"
 
 void init_vars(t_game *game)
 {
