f38766583e158cbcf19ef89b47786222e3711079
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-04-02 06:46:57 -0300

    Update check_map_parameters.c

--- sources/check_map_parameters.c
+++ sources/check_map_parameters.c
@@ -6,27 +6,31 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 18:09:43 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/24 13:28:44 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/04/02 06:28:44 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../header/libft.h"
-#include "../header/so_long.h"
+#include "so_long.h"
 
 void	check_map_parameters(t_game *game)
 {
 	int	i;
+	int	player;
 
 	i = 0;
+	player = 0;
 	while (i <= game->map.nbr_lines)
 	{
-		if (check_for_invalid_map_parameters(game->map.line[i]) == TRUE)
+		if (check_for_invalid_map_parameters(game->map.line[i]) == true)
 			error_msg("Not expected map parameter");
-		game->map.coins += ft_count_occurrences(game->map.line[i], COINS);
-		game->map.exits += ft_count_occurrences(game->map.line[i], MAP_EXIT);
-		game->map.nbr_players += ft_count_occurrences(game->map.line[i], START_POS);
-		if (game->map.nbr_players == 1)
+		game->map.coins += occurences_in_line(game->map.line[i], COINS);
+		game->map.exits += occurences_in_line(game->map.line[i], MAP_EXIT);
+		game->map.nbr_players += occurences_in_line(game->map.line[i], PLAYER);
+		if (game->map.nbr_players == 1 && player == 0)
+		{
 			set_start_position(&game, game->map.line[i], i);
+			player++;
+		}
 		i++;
 	}
 	if (game->map.coins == 0)
