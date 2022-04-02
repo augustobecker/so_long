42b55c6465f2b3d5ab1323ff936264ed21c40c57
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-04-02 06:55:55 -0300

    Update error_msg.c

--- sources/error_msg.c
+++ sources/error_msg.c
@@ -6,12 +6,11 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 07:08:26 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/10 07:08:48 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/04/02 07:08:48 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../header/libft.h"
-#include "../header/so_long.h"
+#include "so_long.h"
 
 int error_msg(char *message)
 {
