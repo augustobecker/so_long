2b2fceeae2361b1f88589aa252b135386e81162f
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-04-11 15:34:34 -0300

    Update so_long.c

--- prototype/so_long.c
+++ prototype/so_long.c
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 21:16:12 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/03/21 15:40:53 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/04/11 20:33:28 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,31 @@
 # define WALL		'1'
 # define COINS  	'C'
 # define MAP_EXIT  	'E'
-# define START_POS  'P'
+# define PLAYER		'P'
 # define EMPTY_SPC  '0'
+
+# define KEY_W		119
+# define KEY_A		97
+# define KEY_S		115
+# define KEY_D		100
+
+# define KEY_W_u	87
+# define KEY_A_u	65
+# define KEY_S_u	83
+# define KEY_D_u	68
+
+# define KEY_UP  	65362
+# define KEY_LEFT  	65361
+# define KEY_RIGHT 	65363
+# define KEY_DOWN  	65364
+
+# define KEY_Q		113
+# define KEY_ESC  	65307
 
 typedef enum e_bool
 {
-	TRUE = 1,
-	FALSE = 0
+	false,
+	true
 }	t_bool;
 
 typedef struct s_position
@@ -56,11 +74,11 @@ typedef struct s_image
 typedef struct s_map
 {
 	char		**line;
-	int			nbr_lines;
-	int			line_len;
+	int			lines;
+	int			columns;
 	int 		coins;
 	int 		exit;
-	int			nbr_players;
+	int			players;
 	t_positon	player;
 }	t_map;
 
@@ -71,7 +89,7 @@ typedef struct s_game
 	int 		movements;
 	t_map		map;
 	t_image		wall;
-	t_image		player_one;
+	t_image		player;
 	t_image		floor;
 	t_image		coins;
 	t_image		exit;
@@ -83,25 +101,47 @@ static int handle_no_event(void *game)
 	return (0);
 }
 
-static int handle_input(int keysym, t_game *game)
-{
-	if ( keysym == XK_Escape)
-		mlx_destroy_window(game->mlx_ptr, game->win_ptr);
-	return (0);
-}
-
-
 int error_msg(char *message)
 {
 	ft_printf(RED"Error\n"GREY"%s\n"RESET, message);
 	exit (EXIT_FAILURE);
 }
 
+void player_move(t_game *game, int line, int column)
+{
+	if (line == 0 || column == 0
+			|| line < game->map.lines || column < game->map.columns)
+		return ;
+	return ;
+}
+
+int handle_input(int keysym, t_game *game)
+{
+	int	line;
+	int	column;
+	
+	line = game->map.player.x;
+	column = game->map.player.y;
+	if (keysym == KEY_Q || keysym == KEY_ESC)
+		//free_game(t_game *game);
+		return (0);
+	if (keysym == KEY_UP || keysym == KEY_W)
+		player_move(game, line, --column);
+	if (keysym == KEY_LEFT || keysym == KEY_A)
+		player_move(game, --line, column);
+	if (keysym == KEY_RIGHT || keysym == KEY_D)
+		player_move(game, ++line, column);
+	if (keysym == KEY_DOWN || keysym == KEY_S)
+		player_move(game, line, ++column);
+	return (0);
+}
+
+
 void init_vars(t_game *game)
 {
 	game->map.coins = 0;
 	game->map.exit = 0;
-	game->map.nbr_players = 0;
+	game->map.players = 0;
 	game->movements = 0;
 }
 
@@ -110,7 +150,7 @@ size_t strlen_line(char *str)
 	size_t	length;
 
 	length = 0;
-	while ((str[length]) && (ft_isprint(str[length]) == TRUE))
+	while (str[length] && ft_isprint(str[length]))
 		length++;
 	return (length);
 }
@@ -119,7 +159,7 @@ void check_command_line_arguments(int argc, char **argv)
 {
 	int map_parameter_len;
 
-	if (argc > 2)
+	if (argc > 10)
 		error_msg("Too many arguments (It should be only two).");
 	if (argc < 2)
 		error_msg("The Map file is missing");
@@ -133,18 +173,18 @@ t_bool check_for_invalid_map_parameters(char *line)
 	int parameter;
 
 	parameter = 0;
-	while ((line[parameter]) && ft_isprint(line[parameter]) == TRUE)
+	while ((line[parameter]) && ft_isprint(line[parameter]) == true)
 	{
 		if ((line[parameter] == WALL) 
 			|| (line[parameter] == EMPTY_SPC)
 			|| (line[parameter] == COINS) 
 			|| (line[parameter] == MAP_EXIT)
-			|| (line[parameter] == START_POS))
+			|| (line[parameter] == PLAYER))
 			parameter++;
 		else
-			return (TRUE);
+			return (true);
 	}
-	return (FALSE);
+	return (false);
 }
 
 void init_map(t_game *game, char *argv)
@@ -169,9 +209,9 @@ void init_map(t_game *game, char *argv)
 	}
 	close(map_fd);
 	game->map.line = ft_split(temporary, '\n');
-	game->map.nbr_lines = ft_count_occurrences(temporary, '\n');
-	game->map.nbr_lines++;
-	game->map.line_len = strlen_line(game->map.line[0]);
+	game->map.lines = ft_count_occurrences(temporary, '\n');
+	game->map.lines++;
+	game->map.columns = strlen_line(game->map.line[0]);
 }
 
 
@@ -181,7 +221,7 @@ void set_start_position(t_game *game, char *line, int y)
 	int x;
 
 	x = 0;
-	while(line[x] && line[x] != START_POS)
+	while(line[x] && line[x] != PLAYER)
 		x++;
 	if (line[x] == '\0')
 		return ;
@@ -212,14 +252,14 @@ void	check_map_parameters(t_game *game)
 	int	i;
 
 	i = 0;
-	while (i < game->map.nbr_lines)
+	while (i < game->map.lines)
 	{
 		if (check_for_invalid_map_parameters(game->map.line[i]) == 1)
 			error_msg("Not expected map parameter");
 		game->map.coins += count_occurrences(game->map.line[i], COINS);
 		game->map.exit += count_occurrences(game->map.line[i], MAP_EXIT);
-		game->map.nbr_players += count_occurrences(game->map.line[i], START_POS);
-		if (game->map.nbr_players == 1)
+		game->map.players += count_occurrences(game->map.line[i], PLAYER);
+		if (game->map.players == 1)
 			set_start_position(game, game->map.line[i], i);
 		i++;
 	}
@@ -227,7 +267,7 @@ void	check_map_parameters(t_game *game)
 		error_msg("Invalid Map. There are no collectibles!");
 	else if (game->map.exit == 0)
 		error_msg("Invalid Map. There is no Exit");
-	else if (game->map.nbr_players != 1)
+	else if (game->map.players != 1)
 		error_msg("Invalid Map. Invalid player quantity.");
 }
 
@@ -236,24 +276,23 @@ void check_map(t_game *game)
 	int 	i;
 
 	i = 0;
-
-	if (game->map.nbr_lines == game->map.line_len)
-		error_msg("Square Map. The Map must be rectangular!");	
-	while(i < game->map.nbr_lines)
+	while(i < game->map.lines)
 	{
-		if ((int) strlen_line(game->map.line[i]) != game->map.line_len)
+		if ((int) strlen_line(game->map.line[i]) != game->map.columns)
 			error_msg("The Map must be rectangular!");
 		if ((game->map.line[i][0] != WALL)
-			|| (game->map.line[i][game->map.line_len - 1] != WALL))
-			error_msg("The Map must be surrounded by walls! Missing vertical wall.");
+			|| (game->map.line[i][game->map.columns - 1] != WALL))
+			error_msg("The Map must be surrounded by walls! \
+				A vertical wall is missing.");
 		i++;
 	}
 	i = 0;
-	while (i < game->map.line_len)
+	while (i < game->map.columns)
 	{
 		if ((game->map.line[0][i] != WALL) 
-			|| (game->map.line[game->map.nbr_lines - 1][i] != WALL))
-			error_msg("The Map must be surrounded by walls! Missing horizontal wall.");
+			|| (game->map.line[game->map.lines - 1][i] != WALL))
+			error_msg("The Map must be surrounded by walls! \
+				A horizontal wall is missing.");
 		i++;
 	}
 	check_map_parameters(game);
@@ -266,9 +305,9 @@ void print_map(t_game *game)
 
 	i = 0;
 	j = 0;
-	while (j < game->map.nbr_lines)
+	while (j < game->map.lines)
 	{
-		while (i < game->map.line_len)
+		while (i < game->map.columns)
 		{
 			if (game->map.line[j][i] == WALL)
 				mlx_put_image_to_window (game->mlx_ptr, game->win_ptr, game->wall.xpm_ptr, i * game->wall.width, j * game->wall.height);
@@ -278,6 +317,8 @@ void print_map(t_game *game)
 				mlx_put_image_to_window (game->mlx_ptr, game->win_ptr, game->coins.xpm_ptr, i * game->coins.width, j * game->coins.height);
 			if (game->map.line[j][i] == MAP_EXIT)
 				mlx_put_image_to_window (game->mlx_ptr, game->win_ptr, game->exit.xpm_ptr, i * game->exit.width, j * game->exit.height);
+			if (game->map.line[j][i] == PLAYER)
+				mlx_put_image_to_window (game->mlx_ptr, game->win_ptr, game->player.xpm_ptr, i * game->player.width, j * game->player.height);
 			i++;
 		}
 		i = 0; 
@@ -285,13 +326,20 @@ void print_map(t_game *game)
 	}
 }
 
+void print_player(t_game *game, int pos_x, int pos_y)
+{
+	mlx_put_image_to_window (game->mlx_ptr, game->win_ptr, game->floor.xpm_ptr, pos_x * game->floor.width, pos_y * game->floor.height);
+	mlx_put_image_to_window (game->mlx_ptr, game->win_ptr, game->player.xpm_ptr, (pos_x + 1) * game->player.width, pos_y * game->player.height);
+}
+
+
 void init_game(t_game *game)
 {
 	game->mlx_ptr = mlx_init();
 	if (game->mlx_ptr == NULL)
-		error_msg("Couldn't find mlx pointer.");
+		error_msg("Couldn't find mlx pointer. Try it using a VNC.");
 	game->win_ptr = mlx_new_window(game->mlx_ptr, \
-	game->map.line_len * 32, game->map.nbr_lines * 32, "so_long");
+	game->map.columns * 32, game->map.lines * 32, "so_long");
 	if (game->win_ptr == NULL)
 		error_msg("Couldn't create the Window.");
 	game->wall.xpm_ptr = mlx_xpm_file_to_image(game->mlx_ptr, \
@@ -302,18 +350,32 @@ void init_game(t_game *game)
 	"folder/assets/images/coin-bag.xpm", &game->coins.width, &game->coins.height);
 	game->exit.xpm_ptr = mlx_xpm_file_to_image(game->mlx_ptr, \
 	"folder/assets/images/exit-ladder.xpm", &game->exit.width, &game->exit.height);
+	game->player.xpm_ptr = mlx_xpm_file_to_image(game->mlx_ptr, \
+	"folder/assets/images/player.xpm", &game->player.width, &game->player.height);
 }
 
 int	main(int argc, char **argv)
 {
 	t_game	game;
-
+	int i;
+	
 	check_command_line_arguments(argc, argv);
 	init_vars(&game);
 	init_map(&game, argv[1]);
 	check_map(&game);
+	i = 0;
+	while (i <= game.map.lines)
+		ft_putendl_fd(game.map.line[i++], 1);
 	init_game(&game);
 	print_map(&game);
+	/*i = 0;
+	while (i < 100000)
+		ft_printf("%d\n", i++);
+	print_player(&game, game.map.player.x, game.map.player.y);
+	i = 0;
+	while (i < 100000)
+		ft_printf("%d\n", i++);
+	print_player(&game, game.map.player.x + 1, game.map.player.y);*/
 	mlx_loop_hook(game.mlx_ptr, &handle_no_event, &game);
 	mlx_loop_hook(game.mlx_ptr, &handle_input, &game);
 	mlx_loop(game.mlx_ptr);
