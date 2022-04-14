186e1a249db8c0668a93f882a4c5ce1f75c1d94f
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-04-14 18:25:53 -0300

    Update so_long.h

--- sources/so_long.h
+++ sources/so_long.h
@@ -27,6 +27,21 @@
 # define MAP_EXIT  	'E'
 # define PLAYER 	'P'
 
+# define KEY_W		119
+# define KEY_A		97
+# define KEY_S		115
+# define KEY_D		100
+
+# define KEY_UP  	65362
+# define KEY_LEFT  	65361
+# define KEY_RIGHT 	65363
+# define KEY_DOWN  	65364
+
+# define KEY_Q		113
+# define KEY_ESC  	65307
+
+# define HEADER		25
+
 typedef enum e_bool
 {
 	false,
