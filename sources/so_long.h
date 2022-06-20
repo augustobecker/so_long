acc832d73a8754329234f97c60a78c6128e67379
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-06-20 17:14:26 -0300

    Update so_long.h

--- sources/so_long.h
+++ sources/so_long.h
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 13:53:38 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/04/04 11:36:58 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/06/20 16:36:58 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,24 +33,22 @@
 # define RESET 			"\033[0m"
 
 # define WALL			'1'
-# define COINS  		'C'
-# define MAP_EXIT 	 	'E'
-# define PLAYER			'P'
 # define FLOOR 			'0'
+# define COINS  		'C'
+# define PLAYER			'P'
+# define MAP_EXIT 		'E'
 # define TOXIC			'T'
-# define SPIKES			'S'
 
-# define WALL_XPM		"assets/folder/assets/sprites/wall.xpm"
-# define COINS_XPM		"assets/folder/assets/sprites/coin-bag.xpm"
-# define OPEN_EXIT_XPM		"assets/folder/assets/sprites/open-exit.xpm"
-# define EXIT_CLOSED_XPM	"assets/folder/assets/sprites/exit-closed.xpm"
-# define PLAYER_FRONT_XPM	"assets/folder/assets/sprites/player/front.xpm"
-# define PLAYER_LEFT_XPM	"assets/folder/assets/sprites/player/left.xpm"
-# define PLAYER_RIGHT_XPM	"assets/folder/assets/sprites/player/right.xpm"
-# define PLAYER_BACK_XPM	"assets/folder/assets/sprites/player/back.xpm"
-# define FLOOR_XPM		"assets/folder/assets/sprites/floor.xpm"
-# define TOXIC_XPM		"assets/folder/assets/sprites/toxic-river.xpm"
-# define SPIKES_XPM		"assets/folder/assets/sprites/spikes.xpm"
+# define WALL_XPM		"../assets/sprites/wall.xpm"
+# define FLOOR_XPM		"../assets/sprites/floor.xpm"
+# define COINS_XPM		"../assets/sprites/coin-bag.xpm"
+# define PLAYER_FRONT_XPM	"../assets/sprites/player/front.xpm"
+# define PLAYER_LEFT_XPM	"../assets/sprites/player/left.xpm"
+# define PLAYER_RIGHT_XPM	"../assets/sprites/player/right.xpm"
+# define PLAYER_BACK_XPM	"../assets/sprites/player/back.xpm"
+# define OPEN_EXIT_XPM		"../assets/sprites/open-exit.xpm"
+# define EXIT_CLOSED_XPM	"../assets/sprites/exit-closed.xpm"
+# define TOXIC_XPM		"../assets/sprites/toxic-river.xpm"
 
 # define KEY_W			119
 # define KEY_A			97
@@ -85,8 +83,8 @@ typedef struct s_position
 typedef struct s_image
 {
 	void	*xpm_ptr;
-	int	width;
-	int	height;
+	int	x;
+	int	y;
 } t_image;
 
 typedef struct s_map
@@ -110,27 +108,26 @@ typedef struct s_game
 	t_image		wall;
 	t_image		floor;
 	t_image		coins;
-	t_image		exit_open;
+	t_image		open_exit;
 	t_image		exit_closed;
 	t_image		player_right;
 	t_image		player_left;
 	t_image		player_back;
 	t_image		player_front;
 	t_image		toxic;
-	t_image		spikes;
 }	t_game;
 
+void	ft_check_command_line_arguments(int argc, char **argv);
+void	ft_init_vars(t_game *game);
+void	ft_init_map(t_game *game, char *argv);
+void	ft_check_map(t_game *game);
+void	ft_check_map_parameters(t_game *game);
+t_bool	ft_check_for_invalid_map_parameters(char *line);
+void	ft_init_mlx(t_game *game);
+void	ft_init_sprites(t_game *game);
 
-void	check_command_line_arguments(int argc, char **argv);
-void	init_map(t_game *game, char *argv);
-void	init_vars(t_game game);
-void	check_map(t_game *game);
-void	check_map_parameters(t_game *game);
-e_bool	check_for_invalid_map_parameters(char *line);
-void	set_start_position(t_game *game, char *line, int y);
-size_t	strlen_line(char *str);
-int	occurences_in_line(char *str, char c);
-int	error_msg(char *message);
-void	init_game(t_game *game);
+size_t	ft_strlen_line(char *str);
+int	ft_count_line_occurrences(char *str, char c);
+void	ft_set_start_position(t_game *game, char *line, int y);
 
 #endif
