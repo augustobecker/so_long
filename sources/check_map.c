02d373452bd68e548407dc03a90353f73ad0e2f4
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-07-13 22:47:28 -0300

    Update check_map.c

--- sources/check_map.c
+++ sources/check_map.c
@@ -1,16 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check_map.c                                        :+:      :+:    :+:   */
+/*   ft_check_map.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/09 23:17:55 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/06/27 16:56:50 by acesar-l         ###   ########.fr       */
+/*   Created: 2022/07/14 03:43:34 by acesar-l          #+#    #+#             */
+/*   Updated: 2022/07/14 03:46:14 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
+void	ft_check_map(t_game *game);
+void	ft_check_rows(t_game *game);
+void	ft_check_columns(t_game *game);
+void	ft_count_map_parameters(t_game *game);
+void	ft_verify_map_parameters(t_game *game);
+
+void	ft_check_map(t_game *game)
+{
+	ft_check_rows(game);
+	ft_check_columns(game);
+	ft_count_map_parameters(game);
+	ft_verify_map_parameters(game);
+}
 
 void	ft_check_rows(t_game *game)
 {
@@ -19,16 +31,16 @@ void	ft_check_rows(t_game *game)
 	i = 0;
 	while (i < game->map.rows)
 	{
-		if ((int) ft_strlen_line(game->map.full[i]) != game->map.columns)
-			ft_error_msg("Invalid Map. The Map must be rectangular!");
-		if (game->map.full[0][i] != WALL)
+		if ((int) ft_strlen(game->map.full[i]) != game->map.columns)
+			ft_error_msg("Invalid Map. The Map must be rectangular!", game);
+		else if (game->map.full[i][0] != WALL)
 			ft_error_msg("Invalid Map. \
-			There's a Wall missing from the first row. \
-			The Map must be surrounded by walls!.");
-		if (game->map.full[game->map.rows - 1][i] != WALL)
+There's a Wall missing from the first row.\n\
+The Map must be surrounded by walls!.", game);
+		else if (game->map.full[i][game->map.columns - 1] != WALL)
 			ft_error_msg("Invalid Map. \
-			There's a Wall missing from the last row. \
-			The Map must be surrounded by walls!.");
+There's a Wall missing from the last row.\n\
+The Map must be surrounded by walls!.", game);
 		i++;
 	}
 }
@@ -38,53 +50,56 @@ void	ft_check_columns(t_game *game)
 	int	i;
 
 	i = 0;
-	while (i < game->map.rows)
+	while (i < game->map.columns)
 	{
-		if (game->map.full[i][0] != WALL)
-			ft_error_msg("Invalid Map. 
-			There's a Wall missing from the first column. \
-			The Map must be surrounded by walls!.");
-		if (game->map.full[i][game->map.rows - 1] != WALL)
-			ft_error_msg("Invalid Map. 
-			There's a Wall missing from the last column. \
-			The Map must be surrounded by walls!.");
+		if (game->map.full[0][i] != WALL)
+			ft_error_msg("Invalid Map. \
+There's a Wall missing from the first column.\n\
+The Map must be surrounded by walls!.", game);
+		else if (game->map.full[game->map.rows - 1][i] != WALL)
+			ft_error_msg("Invalid Map. \
+There's a Wall missing from the last column.\n\
+The Map must be surrounded by walls!.", game);
 		i++;
 	}
 }
 
 void	ft_count_map_parameters(t_game *game)
 {
-	int	i;
+	int	x;
+	int	y;
 
-	i = 0;
-	while (i < game->map.rows)
+	y = 0;
+	while (y < game->map.rows)
 	{
-		if (ft_strchr("CEP01", game->map.full[i]))
-			ft_error_msg("Invalid Map. Not expected map parameter");
-		game->map.coins += line_occurrences(game->map.full[i], COINS);
-		game->map.exit += line_occurrences(game->map.full[i], MAP_EXIT);
-		game->map.players += line_occurrences(game->map.full[i], PLAYER);
-		if (game->map.players == 1)
-			ft_set_start_position(game, game->map.full[i], i);
-		i++;
+		x = 0;
+		while (x < game->map.columns)
+		{
+			if (!ft_strchr("CEP01", game->map.full[y][x]))
+				ft_error_msg("Invalid Map. Not expected map parameter.", game);
+			else if (game->map.full[y][x] == PLAYER)
+			{
+				game->map.players++;
+				game->map.player.x = x;
+				game->map.player.y = y;
+			}
+			else if (game->map.full[y][x] == COINS)
+				game->map.coins++;
+			else if (game->map.full[y][x] == MAP_EXIT)
+				game->map.exit++;
+			x++;
+		}
+		y++;
 	}
 }
 
 void	ft_verify_map_parameters(t_game *game)
 {
 	if (game->map.coins == 0)
-		ft_error_msg("Invalid Map. There are no Coins!");
+		ft_error_msg("Invalid Map. There are no Coins!", game);
 	else if (game->map.exit == 0)
-		ft_error_msg("Invalid Map. There is no Exit");
+		ft_error_msg("Invalid Map. There is no Exit.", game);
 	else if (game->map.players != 1)
 		ft_error_msg("Invalid Map. Invalid Player quantity. \
-		It's a single player game.");
-}
-
-void	ft_check_map(t_game *game)
-{
-	ft_check_rows(game);
-	ft_check_columns(game);
-	ft_count_map_parameters(game);
-	ft_verify_map_parameters(game);
+It's a single player game.", game);
 }
