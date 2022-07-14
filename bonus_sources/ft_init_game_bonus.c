6663fd60f128343bf3db1a9c076f1590a498df45
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-07-14 00:46:25 -0300

    Create ft_init_game_bonus.c

---
+++ bonus_sources/ft_init_game_bonus.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_init_game_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/14 04:07:53 by acesar-l          #+#    #+#             */
+/*   Updated: 2022/07/14 05:46:02 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+void	ft_init_vars(t_game *game);
+void	ft_init_mlx(t_game *game);
+void	ft_init_sprites(t_game *game);
+t_image	ft_new_sprite(void *mlx, char *path, t_game *game);
+
+void	ft_init_vars(t_game *game)
+{
+	game->map.coins = 0;
+	game->map.exit = 0;
+	game->map.players = 0;
+	game->movements = 0;
+	game->map.columns = ft_strlen(game->map.full[0]);
+	game->player_sprite = RIGHT;
+}
+
+void	ft_init_mlx(t_game *game)
+{
+	game->mlx_ptr = mlx_init();
+	if (game->mlx_ptr == NULL)
+		ft_error_msg("Couldn't find mlx pointer. Try it using a VNC.", game);
+	game->win_ptr = mlx_new_window(game->mlx_ptr, \
+	game->map.columns * IMG_WIDTH, game->map.rows * IMG_HEIGHT, "so_long");
+	if (game->win_ptr == NULL)
+	{
+		free(game->mlx_ptr);
+		ft_error_msg("Couldn't create the Window.", game);
+	}
+}
+
+void	ft_init_sprites(t_game *game)
+{
+	void	*mlx;
+
+	mlx = game->mlx_ptr;
+	game->wall = ft_new_sprite(mlx, WALL_XPM, game);
+	game->floor = ft_new_sprite(mlx, FLOOR_XPM, game);
+	game->coins = ft_new_sprite(mlx, COINS_XPM, game);
+	game->player_front = ft_new_sprite(mlx, PLAYER_FRONT_XPM, game);
+	game->player_left = ft_new_sprite(mlx, PLAYER_LEFT_XPM, game);
+	game->player_right = ft_new_sprite(mlx, PLAYER_RIGHT_XPM, game);
+	game->player_back = ft_new_sprite(mlx, PLAYER_BACK_XPM, game);
+	game->open_exit = ft_new_sprite(mlx, OPEN_EXIT_XPM, game);
+	game->exit_closed = ft_new_sprite(mlx, EXIT_CLOSED_XPM, game);
+	game->toxic = ft_new_sprite(mlx, TOXIC_RIVER_XPM, game);
+}
+
+t_image	ft_new_sprite(void *mlx, char *path, t_game *game)
+{
+	t_image	sprite;
+
+	sprite.xpm_ptr = mlx_xpm_file_to_image(mlx, path, &sprite.x, &sprite.y);
+	if (sprite.xpm_ptr == NULL)
+		ft_error_msg("Couldn't find a sprite. Does it exist?", game);
+	return (sprite);
+}
