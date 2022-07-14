ee0d4f846f7d8393e072ccb09c077469e2867426
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-07-14 00:43:31 -0300

    Update ft_check_map_bonus.c

--- bonus_sources/ft_check_map_bonus.c
+++ bonus_sources/ft_check_map_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
+#include "so_long_bonus.h"
 
 void	ft_check_map(t_game *game);
 void	ft_check_rows(t_game *game);
